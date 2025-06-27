58cbc26185f0ea1cad506e823a86c5d542c5fb8c
Suman Kumar <suman.k9709@gmail.com>
2025-06-27 15:04:15 +0530

    made contact page responsive

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ContactCard from '../Components/ContactCard'
+import React from 'react';
+import ContactCard from '../Components/ContactCard';
 
 const Contact = () => {
-
-
   return (
-    <div className="w-full px-4 py-12  text-white" id='contact'>
+    <div className="w-full px-4 py-12 text-white" id="contact">
+      {/* Heading */}
       <div className="flex justify-center">
         <h1 className="relative text-3xl font-bold mb-10 group inline-block">
           Contact Us
@@ -13,32 +12,29 @@ const Contact = () => {
         </h1>
       </div>
 
-      <div className='flex w-full justify-around items-center'>
+    
+      <div className="w-full flex flex-col sm:flex-row flex-wrap justify-center items-center gap-6">
         <ContactCard
           name="LinkedIn"
           description="Connect with me professionally and view my career journey."
           logoLink="/Image/linkedin.png"
           link="https://www.linkedin.com/in/suman9709/"
         />
-
         <ContactCard
           name="Gmail"
           description="Drop me an email for collaboration or queries."
           logoLink="/Image/gmail.png"
           link="mailto:suman.k9709@gmail.com"
         />
-
         <ContactCard
           name="GitHub"
           description="Explore my open-source projects and code contributions."
           logoLink="/Image/github.png"
           link="https://github.com/Suman9709"
         />
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
